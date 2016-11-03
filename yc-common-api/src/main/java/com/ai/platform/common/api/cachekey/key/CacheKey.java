27ee33ea9a77df5d0e944d3dc93755d1c07ebb10
houg <hougang_ahut@163.com>
2016-11-03 10:44:50 +0800

    cachekey

---
+++ yc-common-api/src/main/java/com/ai/platform/common/api/cachekey/key/CacheKey.java
@@ -0,0 +1,45 @@
+package com.ai.platform.common.api.cachekey.key;
+
+import java.io.Serializable;
+
+/**
+ * @Description: 缓存键值
+ * @author hougang@asiainfo.com
+ * @date 2016年11月3日 上午10:35:19 
+ * @version V1.0
+ */
+public class CacheKey implements Serializable {
+
+	private static final long serialVersionUID = 3633287499459454094L;
+	
+	/**
+	 * 中文语言对
+	 */
+	public static final String CN_DUAD_KEY = "CN_DUAD";
+	
+	/**
+	 * 英文语言对
+	 */
+	public static final String EN_DUAD_KEY = "EN_DUAD";
+	
+    /**
+     * 中文用途
+     */
+    public static final String CN_PURPOSE_KEY = "CN_DUAD";
+	
+    /**
+     * 英文用途
+     */
+	public static final String EN_PURPOSE_KEY = "EN_PURPOSE";
+	
+	/**
+     * 中文领域
+     */
+    public static final String CN_DOMAIN_KEY = "CN_DOMAIN";
+	
+	/**
+	 * 英文领域
+	 */
+	public static final String EN_DOMAIN_KEY = "EN_DOMAIN";
+
+}
