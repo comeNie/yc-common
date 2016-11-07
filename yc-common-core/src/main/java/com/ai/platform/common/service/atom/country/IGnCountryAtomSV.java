66fb504b3a2360c925a218149bf6855f03ec7709
xuyw <xuyw@xuyw-PC>
2016-11-07 13:39:18 +0800

    提交国家查询

---
+++ yc-common-core/src/main/java/com/ai/platform/common/service/atom/country/IGnCountryAtomSV.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ai.platform.common.service.atom.country;
+
+import java.util.List;
+
+import com.ai.platform.common.dao.mapper.bo.GnCountry;
+import com.ai.platform.common.dao.mapper.bo.GnCountryCriteria;
+
+/**
+ * 国家查询原子层<br>
+ * Date: 2016年11月7日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * 
+ * @author xuyw
+ */
+public interface IGnCountryAtomSV {
+	public List<GnCountry> queryCountry(GnCountryCriteria criteria);
+}
---
+++ yc-common-core/src/test/java/com/ai/platform/common/test/country/CountryTest.java
@@ -0,0 +1,25 @@
+package com.ai.platform.common.test.country;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ai.platform.common.api.country.interfaces.IGnCountrySV;
+import com.ai.platform.common.api.country.param.CountryRequest;
+import com.alibaba.fastjson.JSON;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/context/core-context.xml" })
+public class CountryTest {
+	@Autowired
+	IGnCountrySV gnCountrySV;
+
+	@Test
+	public void testQuery() {
+		CountryRequest request = new CountryRequest();
+		System.err
+				.println(JSON.toJSONString(gnCountrySV.queryCountry(request)));
+	}
+}
