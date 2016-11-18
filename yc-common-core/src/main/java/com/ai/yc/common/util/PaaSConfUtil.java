3b20d4f207799fc0c808b5a2270f88a64b7d6c3c
gucl <gucl@asiainfo.com>
2016-11-18 21:06:24 +0800

    paas-conf添加机房信息

---
+++ yc-common-core/src/main/java/com/ai/yc/common/util/PaaSConfUtil.java
@@ -0,0 +1,68 @@
+package com.ai.yc.common.util;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * SSO配置文件工具类
+ *
+ * Date: 2015年11月29日 <br>
+ * Copyright (c) 2015 asiainfo.com <br>
+ * 
+ * @author gucl
+ */
+public final class PaaSConfUtil {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PaaSConfUtil.class);
+
+    //paas-conf.properties属性文件
+    private static Properties prop = new Properties();
+    
+    public static Properties getProp() {
+		return prop;
+	}
+	public static void setProp(Properties prop) {
+		PaaSConfUtil.prop = prop;
+	}
+	private PaaSConfUtil() {
+    }
+
+    static {
+        try {
+
+            InputStream inStream = PaaSConfUtil.class.getClassLoader().getResourceAsStream(
+                    "paas/paas-conf.properties");
+            prop.load(inStream);
+            LOG.debug("加载paas-conf.properties完毕");
+        } catch (Exception e) {
+            LOG.error("加载paas-conf.properties失败，具体原因：" + e.getMessage(), e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return prop.getProperty(key, "").trim();
+    }
+    
+    public static String getCurrSrvArea(){
+    	return getProperty("srvarea.curr");
+    }
+    
+    public static String[] getAllSrvArea(){
+    	String allAreas=getProperty("srvarea.all");
+    	String[] allAreaArr=allAreas.split(",");
+    	return allAreaArr;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(getProperty("srvarea.all"));
+		System.out.println(getProperty("srvarea.curr"));
+	}
+
+}
