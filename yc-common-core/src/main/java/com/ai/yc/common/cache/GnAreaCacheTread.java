c593136301a77361f211b2a701f2ec30cefd9782
gucl <gucl@asiainfo.com>
2016-11-21 12:58:56 +0800

    同时写多份缓存

--- yc-common-core/src/main/java/com/ai/yc/common/cache/GnAreaCacheTread.java
+++ yc-common-core/src/main/java/com/ai/yc/common/cache/GnAreaCacheTread.java
@@ -1,25 +1,35 @@
 package com.ai.yc.common.cache;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ai.paas.ipaas.mcs.interfaces.ICacheClient;
 import com.ai.yc.common.constants.CacheNSMapper;
 import com.ai.yc.common.dao.mapper.bo.GnArea;
 import com.ai.yc.common.util.CacheFactoryUtil;
+import com.ai.yc.common.util.PaaSConfUtil;
 import com.alibaba.fastjson.JSON;
 
 public class GnAreaCacheTread extends Thread{
     private List<GnArea> resultList;
-    ICacheClient cacheClient = CacheFactoryUtil.getCacheClient(CacheNSMapper.CACHE_GN_AREA);
-    
+    List<ICacheClient> cacheClientList=new ArrayList<ICacheClient>();
     public GnAreaCacheTread(List<GnArea> resultList){
         this.resultList=resultList;
+        String[] areas=PaaSConfUtil.getAllSrvArea();
+        if(areas!=null&&areas.length>0){
+        	for(String srvarea:areas){
+        		ICacheClient cacheClient=CacheFactoryUtil.getCacheClient(srvarea+"."+CacheNSMapper.CACHE_GN_AREA);
+        		cacheClientList.add(cacheClient);
+        	}
+        }
     }
     @Override
     public void run() {
         for (GnArea area : resultList) {          
             String key=area.getAreaCode().toUpperCase();
-            cacheClient.hset(CacheNSMapper.CACHE_GN_AREA, key, JSON.toJSONString(area));
+            for(ICacheClient cacheClient:cacheClientList){
+            	cacheClient.hset(CacheNSMapper.CACHE_GN_AREA, key, JSON.toJSONString(area));
+            }
         } 
         //System.out.println("【"+Thread.currentThread().getName()+"】");
     } 
