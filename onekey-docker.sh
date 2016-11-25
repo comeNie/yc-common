76288ff5a4f9cf4d076938d590ed10b02712f5d3
houg <hougang_ahut@163.com>
2016-11-25 14:22:56 +0800

    docker

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -3,10 +3,10 @@ git reset --hard origin/master
 git pull 
 chmod a+x onekey-docker.sh 
 gradle clean && gradle build -x test 
-docker build -t 10.19.13.18:5000/yc-common:${tagversion} .   
-docker push 10.19.13.18:5000/yc-common:${tagversion} 
+docker build -t 10.19.13.20:5000/yc-common:${tagversion} .   
+docker push 10.19.13.20:5000/yc-common:${tagversion} 
 
 docker rmi aioptapp/yc-common:${tagversion} 
-docker tag 10.19.13.18:5000/yc-common:${tagversion} aioptapp/yc-common:${tagversion} 
+docker tag 10.19.13.20:5000/yc-common:${tagversion} aioptapp/yc-common:${tagversion} 
 docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
 docker push aioptapp/yc-common:${tagversion} 
