a4da020330a7b1606709587c4575db6d3a78ab3f
shancc <shancc@lenovo-pc>
2017-05-16 13:16:50 +0800

    scc

--- yc-common-api/src/main/java/com/ai/yc/common/api/sysconfig/param/SaveSysConfig.java
+++ yc-common-api/src/main/java/com/ai/yc/common/api/sysconfig/param/SaveSysConfig.java
@@ -4,307 +4,333 @@ import java.io.Serializable;
 
 public class SaveSysConfig implements Serializable {
 	private static final long serialVersionUID = 1L;
-	/**
-	 * 编码ID
-	 */
-	private String id;
-	/**
-	 * 普通会员等级值
-	 */
-    private Integer ordinayryMember;
-    /**
-	 * 黄金会员等级值
-	 */
-    private Integer goldMember;
-    /**
-	 * 白金会员等级值
-	 */
-    private Integer platinumMember;
-    /**
-	 * 钻石会员等级值
-	 */
-    private Integer masonyMember;
-    /**
-	 * 封顶值
-	 */
-    private Integer capValue;
-    /**
-	 * V1译员
-	 */
-    private Integer v1Points;
-    /**
-	 * V2译员
-	 */
-    private Integer v2Points;
-    /**
-	 * V3译员
-	 */
-    private Integer v3Points;
-    /**
-	 * lsp团队
-	 */
-    private Integer lspPoints;
-    /**
-	 * 语料数
-	 */
-    private Integer lgdateNum;
-    /**
-	 * 客户数
-	 */
-    private Integer customNum;
-    /**
-	 * 译员数
-	 */
-    private Integer interpreterNum;
-    /**
-	 * 订单数
-	 */
-    private Integer orderNum;
-    /**
-	 * 语种数
-	 */
-    private Integer languageNum;
-    /**
-     * PC端首页公告
-     */
+	private String configId;
+
+    private String ordinaryMember;
+
+    private String goldMember;
+
+    private String platinumMember;
+
+    private String masonryMember;
+
+    private String capValue;
+
+    private String v1Points;
+
+    private String v2Points;
+
+    private String v3Points;
+
+    private String lspPoints;
+
+    private String lgdataNum;
+
+    private String customNum;
+
+    private String interpreterNum;
+
+    private String orderNum;
+
+    private String languageNum;
+
+    private String oneday;
+
+    private String twoday;
+
+    private String threeday;
+
+    private String fourday;
+
+    private String fiveday;
+
+    private String sixday;
+
+    private String sevenday;
+
+    private String eightday;
+
+    private String regOnoff;
+
+    private String regIntegration;
+
+    private String regGrowth;
+
+    private String inviteIntegrati;
+
+    private String rstate;
+
+    private String gstate;
+
+    private String istate;
+
     private String pcNotice;
-    /**
-     * wap端首页公告
-     */
+
     private String wapNotice;
-    
-    /**
-     * 签到送积分ID
-     */
-    private String did;
-    /**
-     * 连续1日送积分数
-     */
-    private Integer oneDay;
-    /**
-     * 连续2日送积分数
-     */
-    private Integer twoDay;
-    /**
-     * 连续3日送积分数
-     */
-    private Integer threeDay;
-    /**
-     * 连续4日送积分数
-     */
-    private Integer fourDay;
-    /**
-     * 连续5日送积分数
-     */
-    private Integer fiveDay;
-    /**
-     * 连续6日送积分数
-     */
-    private Integer sixDay;
-    /**
-     * 连续7日送积分数
-     */
-    private Integer sevenDay;
-    /**
-     * 签到送积分状态
-     */
-    private String dstate;
-    
-    /**
-     * 注册活动ID
-     */
-    private String aid;
-    /**
-     * 活动名称
-     */
-    private String activiceName;
-    /**
-     * 活动送值
-     */
-    private Integer activiceNum;
-    /**
-     * 活动状态
-     */
-    private String astate;
-    
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public Integer getOrdinayryMember() {
-		return ordinayryMember;
-	}
-	public void setOrdinayryMember(Integer ordinayryMember) {
-		this.ordinayryMember = ordinayryMember;
-	}
-	public Integer getGoldMember() {
-		return goldMember;
-	}
-	public void setGoldMember(Integer goldMember) {
-		this.goldMember = goldMember;
-	}
-	public Integer getPlatinumMember() {
-		return platinumMember;
-	}
-	public void setPlatinumMember(Integer platinumMember) {
-		this.platinumMember = platinumMember;
-	}
-	public Integer getMasonyMember() {
-		return masonyMember;
-	}
-	public void setMasonyMember(Integer masonyMember) {
-		this.masonyMember = masonyMember;
-	}
-	public Integer getCapValue() {
-		return capValue;
-	}
-	public void setCapValue(Integer capValue) {
-		this.capValue = capValue;
-	}
-	public Integer getV1Points() {
-		return v1Points;
-	}
-	public void setV1Points(Integer v1Points) {
-		this.v1Points = v1Points;
-	}
-	public Integer getV2Points() {
-		return v2Points;
-	}
-	public void setV2Points(Integer v2Points) {
-		this.v2Points = v2Points;
-	}
-	public Integer getV3Points() {
-		return v3Points;
-	}
-	public void setV3Points(Integer v3Points) {
-		this.v3Points = v3Points;
-	}
-	public Integer getLspPoints() {
-		return lspPoints;
-	}
-	public void setLspPoints(Integer lspPoints) {
-		this.lspPoints = lspPoints;
-	}
-	public Integer getLgdateNum() {
-		return lgdateNum;
-	}
-	public void setLgdateNum(Integer lgdateNum) {
-		this.lgdateNum = lgdateNum;
-	}
-	public Integer getCustomNum() {
-		return customNum;
-	}
-	public void setCustomNum(Integer customNum) {
-		this.customNum = customNum;
-	}
-	public Integer getInterpreterNum() {
-		return interpreterNum;
-	}
-	public void setInterpreterNum(Integer interpreterNum) {
-		this.interpreterNum = interpreterNum;
-	}
-	public Integer getOrderNum() {
-		return orderNum;
-	}
-	public void setOrderNum(Integer orderNum) {
-		this.orderNum = orderNum;
-	}
-	public Integer getLanguageNum() {
-		return languageNum;
-	}
-	public void setLanguageNum(Integer languageNum) {
-		this.languageNum = languageNum;
-	}
-	public String getPcNotice() {
-		return pcNotice;
-	}
-	public void setPcNotice(String pcNotice) {
-		this.pcNotice = pcNotice;
-	}
-	public String getWapNotice() {
-		return wapNotice;
-	}
-	public void setWapNotice(String wapNotice) {
-		this.wapNotice = wapNotice;
-	}
-	public String getDid() {
-		return did;
-	}
-	public void setDid(String did) {
-		this.did = did;
-	}
-	public Integer getOneDay() {
-		return oneDay;
-	}
-	public void setOneDay(Integer oneDay) {
-		this.oneDay = oneDay;
-	}
-	public Integer getTwoDay() {
-		return twoDay;
-	}
-	public void setTwoDay(Integer twoDay) {
-		this.twoDay = twoDay;
-	}
-	public Integer getThreeDay() {
-		return threeDay;
-	}
-	public void setThreeDay(Integer threeDay) {
-		this.threeDay = threeDay;
-	}
-	public Integer getFourDay() {
-		return fourDay;
-	}
-	public void setFourDay(Integer fourDay) {
-		this.fourDay = fourDay;
-	}
-	public Integer getFiveDay() {
-		return fiveDay;
-	}
-	public void setFiveDay(Integer fiveDay) {
-		this.fiveDay = fiveDay;
-	}
-	public Integer getSixDay() {
-		return sixDay;
-	}
-	public void setSixDay(Integer sixDay) {
-		this.sixDay = sixDay;
-	}
-	public Integer getSevenDay() {
-		return sevenDay;
-	}
-	public void setSevenDay(Integer sevenDay) {
-		this.sevenDay = sevenDay;
-	}
-	public String getDstate() {
-		return dstate;
-	}
-	public void setDstate(String dstate) {
-		this.dstate = dstate;
-	}
-	public String getAid() {
-		return aid;
-	}
-	public void setAid(String aid) {
-		this.aid = aid;
-	}
-	public String getActiviceName() {
-		return activiceName;
-	}
-	public void setActiviceName(String activiceName) {
-		this.activiceName = activiceName;
-	}
-	public Integer getActiviceNum() {
-		return activiceNum;
-	}
-	public void setActiviceNum(Integer activiceNum) {
-		this.activiceNum = activiceNum;
-	}
-	public String getAstate() {
-		return astate;
-	}
-	public void setAstate(String astate) {
-		this.astate = astate;
-	}
+
+    private String lstate;
+
+    public String getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(String configId) {
+        this.configId = configId == null ? null : configId.trim();
+    }
+
+    public String getOrdinaryMember() {
+        return ordinaryMember;
+    }
+
+    public void setOrdinaryMember(String ordinaryMember) {
+        this.ordinaryMember = ordinaryMember == null ? null : ordinaryMember.trim();
+    }
+
+    public String getGoldMember() {
+        return goldMember;
+    }
+
+    public void setGoldMember(String goldMember) {
+        this.goldMember = goldMember == null ? null : goldMember.trim();
+    }
+
+    public String getPlatinumMember() {
+        return platinumMember;
+    }
+
+    public void setPlatinumMember(String platinumMember) {
+        this.platinumMember = platinumMember == null ? null : platinumMember.trim();
+    }
+
+    public String getMasonryMember() {
+        return masonryMember;
+    }
+
+    public void setMasonryMember(String masonryMember) {
+        this.masonryMember = masonryMember == null ? null : masonryMember.trim();
+    }
+
+    public String getCapValue() {
+        return capValue;
+    }
+
+    public void setCapValue(String capValue) {
+        this.capValue = capValue == null ? null : capValue.trim();
+    }
+
+    public String getV1Points() {
+        return v1Points;
+    }
+
+    public void setV1Points(String v1Points) {
+        this.v1Points = v1Points == null ? null : v1Points.trim();
+    }
+
+    public String getV2Points() {
+        return v2Points;
+    }
+
+    public void setV2Points(String v2Points) {
+        this.v2Points = v2Points == null ? null : v2Points.trim();
+    }
+
+    public String getV3Points() {
+        return v3Points;
+    }
+
+    public void setV3Points(String v3Points) {
+        this.v3Points = v3Points == null ? null : v3Points.trim();
+    }
+
+    public String getLspPoints() {
+        return lspPoints;
+    }
+
+    public void setLspPoints(String lspPoints) {
+        this.lspPoints = lspPoints == null ? null : lspPoints.trim();
+    }
+
+    public String getLgdataNum() {
+        return lgdataNum;
+    }
+
+    public void setLgdataNum(String lgdataNum) {
+        this.lgdataNum = lgdataNum == null ? null : lgdataNum.trim();
+    }
+
+    public String getCustomNum() {
+        return customNum;
+    }
+
+    public void setCustomNum(String customNum) {
+        this.customNum = customNum == null ? null : customNum.trim();
+    }
+
+    public String getInterpreterNum() {
+        return interpreterNum;
+    }
+
+    public void setInterpreterNum(String interpreterNum) {
+        this.interpreterNum = interpreterNum == null ? null : interpreterNum.trim();
+    }
+
+    public String getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(String orderNum) {
+        this.orderNum = orderNum == null ? null : orderNum.trim();
+    }
+
+    public String getLanguageNum() {
+        return languageNum;
+    }
+
+    public void setLanguageNum(String languageNum) {
+        this.languageNum = languageNum == null ? null : languageNum.trim();
+    }
+
+    public String getOneday() {
+        return oneday;
+    }
+
+    public void setOneday(String oneday) {
+        this.oneday = oneday == null ? null : oneday.trim();
+    }
+
+    public String getTwoday() {
+        return twoday;
+    }
+
+    public void setTwoday(String twoday) {
+        this.twoday = twoday == null ? null : twoday.trim();
+    }
+
+    public String getThreeday() {
+        return threeday;
+    }
+
+    public void setThreeday(String threeday) {
+        this.threeday = threeday == null ? null : threeday.trim();
+    }
+
+    public String getFourday() {
+        return fourday;
+    }
+
+    public void setFourday(String fourday) {
+        this.fourday = fourday == null ? null : fourday.trim();
+    }
+
+    public String getFiveday() {
+        return fiveday;
+    }
+
+    public void setFiveday(String fiveday) {
+        this.fiveday = fiveday == null ? null : fiveday.trim();
+    }
+
+    public String getSixday() {
+        return sixday;
+    }
+
+    public void setSixday(String sixday) {
+        this.sixday = sixday == null ? null : sixday.trim();
+    }
+
+    public String getSevenday() {
+        return sevenday;
+    }
+
+    public void setSevenday(String sevenday) {
+        this.sevenday = sevenday == null ? null : sevenday.trim();
+    }
+
+    public String getEightday() {
+        return eightday;
+    }
+
+    public void setEightday(String eightday) {
+        this.eightday = eightday == null ? null : eightday.trim();
+    }
+
+    public String getRegOnoff() {
+        return regOnoff;
+    }
+
+    public void setRegOnoff(String regOnoff) {
+        this.regOnoff = regOnoff == null ? null : regOnoff.trim();
+    }
+
+    public String getRegIntegration() {
+        return regIntegration;
+    }
+
+    public void setRegIntegration(String regIntegration) {
+        this.regIntegration = regIntegration == null ? null : regIntegration.trim();
+    }
+
+    public String getRegGrowth() {
+        return regGrowth;
+    }
+
+    public void setRegGrowth(String regGrowth) {
+        this.regGrowth = regGrowth == null ? null : regGrowth.trim();
+    }
+
+    public String getInviteIntegrati() {
+        return inviteIntegrati;
+    }
+
+    public void setInviteIntegrati(String inviteIntegrati) {
+        this.inviteIntegrati = inviteIntegrati == null ? null : inviteIntegrati.trim();
+    }
+
+    public String getRstate() {
+        return rstate;
+    }
+
+    public void setRstate(String rstate) {
+        this.rstate = rstate == null ? null : rstate.trim();
+    }
+
+    public String getGstate() {
+        return gstate;
+    }
+
+    public void setGstate(String gstate) {
+        this.gstate = gstate == null ? null : gstate.trim();
+    }
+
+    public String getIstate() {
+        return istate;
+    }
+
+    public void setIstate(String istate) {
+        this.istate = istate == null ? null : istate.trim();
+    }
+
+    public String getPcNotice() {
+        return pcNotice;
+    }
+
+    public void setPcNotice(String pcNotice) {
+        this.pcNotice = pcNotice == null ? null : pcNotice.trim();
+    }
+
+    public String getWapNotice() {
+        return wapNotice;
+    }
+
+    public void setWapNotice(String wapNotice) {
+        this.wapNotice = wapNotice == null ? null : wapNotice.trim();
+    }
+
+    public String getLstate() {
+        return lstate;
+    }
+
+    public void setLstate(String lstate) {
+        this.lstate = lstate == null ? null : lstate.trim();
+    }
 }
